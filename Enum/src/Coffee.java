854c901c101f7c09742225b883497411a1117243
Patorn <PPMoss2537@gmail.com>
2016-07-02 11:55:16 -0400

    Enum and compiler compliance

--- Enum/src/Coffee.java
+++ Enum/src/Coffee.java
@@ -1,4 +1,12 @@
-
 public class Coffee {
-
+	public Coffee() {};
+	private enum coffeeSize{ small, medium, large}
+	coffeeSize size;
+//Demonstrating enum
+//enum is to limit a variable to have predefined values
+	public static void main(String args[]) {
+		Coffee myCoffee = new Coffee();
+		myCoffee.size = Coffee.coffeeSize.large;
+		System.out.println(myCoffee.size);
+	}
 }
