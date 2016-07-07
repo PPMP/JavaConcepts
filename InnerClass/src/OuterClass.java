1b1d666d327bbb91d442d5faed3b42426ff6e9f6
Patorn <PPMoss2537@gmail.com>
2016-07-06 23:56:28 -0400

    Inner Classes

---
+++ InnerClass/src/OuterClass.java
@@ -0,0 +1,32 @@
+/*Demonstrating inner classes
+InnerClass can be static - makes it stand alone class
+can instantiate by calling constructor directly
+Innerclass can access instance variables and methods of super class
+Super class has access to Inner class with instantiation and initialization of inner
+classes constructor*/
+public class OuterClass {
+	private int age = 99;
+	public OuterClass(){
+		System.out.println("Hello");
+		InnerClass inner = new InnerClass();
+		System.out.println(inner.cool);
+	}
+	
+	private void lol(){
+		System.out.println("lol");
+	}
+	
+	private class InnerClass{
+		private int cool = 9;
+		public InnerClass(){
+			System.out.println("Hello its me inner class " + age);
+			lol();
+		}
+	}
+
+	public static void main(String args[]){
+		OuterClass outer = new OuterClass();
+		OuterClass.InnerClass inner = outer.new InnerClass();
+	
+	}
+}
