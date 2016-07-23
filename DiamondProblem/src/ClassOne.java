80e21d90f02ed29a975ae89cf707489450bb1629
Patorn Pongsanit <Patorn@patorns-mbp.attlocal.net>
2016-07-23 16:09:41 -0400

    Accessing using this and super

--- DiamondProblem/src/ClassOne.java
+++ DiamondProblem/src/ClassOne.java
@@ -2,4 +2,5 @@
  * Created by Patorn on 7/22/16.
  */
 public abstract class ClassOne {
+    int age = 18;
 }
--- DiamondProblem/src/DiamondProblem.java
+++ DiamondProblem/src/DiamondProblem.java
@@ -2,8 +2,18 @@
  * Created by Patorn on 7/22/16.
  */
 //public class DiamondProblem extends ClassOne, ClassTwo {
-//
-//}
+public class DiamondProblem extends ClassOne {
+    int age = 21;
+
+    void DisplayAge(){
+        System.out.println("Age in DiamondProblem is: " + this.age);
+        System.out.println("Age in ClassOne is: " + super.age);
+    }
+    public static void main(String args[]){
+        DiamondProblem diamond = new DiamondProblem();
+        diamond.DisplayAge();
+    }
+}
 
 //Java does not allow multiple inheritence because if a class extends two classes,
 //the super class does not know which method to use if the extended classes have
