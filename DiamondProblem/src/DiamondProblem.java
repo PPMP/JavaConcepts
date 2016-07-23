893c3e26ebac649feca80db8a4653c1690a63408
Patorn Pongsanit <Patorn@patorns-mbp.attlocal.net>
2016-07-23 17:07:48 -0400

    Changes to Diamond

--- DiamondProblem/src/DiamondProblem.java
+++ DiamondProblem/src/DiamondProblem.java
@@ -15,6 +15,9 @@ public class DiamondProblem extends ClassOne {
     }
 }
 
-//Java does not allow multiple inheritence because if a class extends two classes,
-//the super class does not know which method to use if the extended classes have
-//the same method. Same for abstract class.
+/** Java does not allow multiple inheritence because if a class extends two classes,
+the super class does not know which method to use if the extended classes have
+the same method. Same for abstract class. This to access fields and methods in subclass. Super
+to access fields and methods of parent class. Superclass reference variable can hold the subclass object, but
+ using that variable you can access only the members of the superclass, so to access the members of both classes it
+ is recommended to always create reference variable to the subclass. **/
